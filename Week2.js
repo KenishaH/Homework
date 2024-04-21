aad12ef216684246cfbadadd639c30761d846b02
Kenisha Hope <kenishahope12@gmail.com>
2024-04-20 21:34:54 -0500

    Week 2 Homework

---
+++ Week2.js
@@ -0,0 +1,11 @@
+let pizzPlace = "Pizza Hut";
+
+let numberOfToppings = 2;
+console.log(typeof pizzPlace);
+console.log(typeof numberOfToppings);
+
+if (numberOfToppings < 10) {
+  console.log("Quality , not quantity");
+} else {
+  console.log("A whole lot of pizza");
+}
